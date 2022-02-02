e01cabe3f859dc042ccc255e8e3c3dea019faa18
Marcello Alves <marcellodoalves@hotmail.com>
2022-02-02 09:31:58 -0300

    add media query file

---
+++ lib/ResponseMediaQuery.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ResponseMediaQuery extends StatefulWidget {
+  const ResponseMediaQuery({ Key? key }) : super(key: key);
+
+  @override
+  _ResponseMediaQueryState createState() => _ResponseMediaQueryState();
+}
+
+class _ResponseMediaQueryState extends State<ResponseMediaQuery> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
