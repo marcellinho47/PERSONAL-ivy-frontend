ac3590f195226fc63989a6aa13c568f035f6ad6e
Marcello Alves <70907544+marcellinho94@users.noreply.github.com>
2022-05-20 10:26:12 -0300

    init client screen

--- lib/screens/client_screen.dart
+++ lib/screens/client_screen.dart
@@ -8,10 +8,82 @@ class ClientScreen extends StatefulWidget {
 }
 
 class _ClientScreenState extends State<ClientScreen> {
+  // ----------------------------------------------------------
+  // VARIABLES
+  // ----------------------------------------------------------
+  TextEditingController _id = TextEditingController();
+  TextEditingController _name = TextEditingController();
+
+  // ----------------------------------------------------------
+  // METHODS
+  // ----------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  double _boxWidth(double _screenWidth) {
+    double loginBoxWidth = _screenWidth;
+
+    if (_screenWidth > 750) {
+      loginBoxWidth = 700;
+    }
+
+    return loginBoxWidth;
+  }
+
+  // ----------------------------------------------------------
+  // BUILD
+  // ----------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("CLIENT"),
+    double _screenWidth = MediaQuery.of(context).size.width;
+
+    return Center(
+      child: SizedBox(
+        width: _boxWidth(_screenWidth),
+        child: Column(
+          children: [
+            Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        controller: _id,
+                        keyboardType: TextInputType.number,
+                        enabled: true,
+                        decoration: const InputDecoration(
+                          hintText: "",
+                          labelText: "Código",
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      flex: 8,
+                      child: TextField(
+                        controller: _name,
+                        keyboardType: TextInputType.text,
+                        enabled: true,
+                        decoration: const InputDecoration(
+                          hintText: "",
+                          labelText: "Descrição",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
